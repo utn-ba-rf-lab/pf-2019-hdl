93aa9a20b285666697510ee1b61c96fa524bd2da
Marcelo <mdoallo@frba.utn.edu.ar>
2022-12-01 08:47:52 -0300

    modified:   modules/ledBlinky/Makefile 	modified:   modules/ledBlinky/out/blinky.blif 	modified:   modules/ledBlinky/out/blinky.json 	modified:   modules/ledBlinky/src/blinky.v

--- modules/ledBlinky/src/blinky.v
+++ modules/ledBlinky/src/blinky.v
@@ -2,19 +2,19 @@
 
 module blinky( 
     // Inputs
-    input piClk,
-    input piRst,
+    input hwclk,
+    input rst,
     //input piEna,
     // Outputs
-    output poLed );
+    output [7:0]leds );
 
     parameter c_bit_counter = 25;
     reg  [c_bit_counter:0] s_counter_reg;
     wire [c_bit_counter:0] s_counter_next;
 
     // Main counter Reg
-    always @ (posedge piClk) begin
-        if ( piRst == 1'b1 ) begin
+    always @ (posedge hwclk) begin
+        if ( rst == 1'b1 ) begin
             s_counter_reg <= {c_bit_counter{1'b0}};
         end
         //else if ( piEna == 1'b1 ) begin
@@ -27,6 +27,6 @@ module blinky(
     assign s_counter_next = s_counter_reg + 1;
 
     // Output logic
-    assign poLed = (s_counter_reg >= {{1'b0}, {(c_bit_counter-1){1'b1}}} ) ? 1'b1 : 1'b0;
+    assign leds[0] = (s_counter_reg >= {{1'b0}, {(c_bit_counter-1){1'b1}}} ) ? 1'b1 : 1'b0;
 
 endmodule
